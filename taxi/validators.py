de73700ec78b0073c76e66a0efd481a79056bd15
Anna <annalepiska@gmail.com>
2024-04-17 18:18:19 +0300

    Fix validator

--- taxi/validators.py
+++ taxi/validators.py
@@ -1,6 +1,6 @@
 from django.core.validators import RegexValidator
 
 license_number_validator = RegexValidator(
-    regex=r"^[A-Z]{3}[0-9]{4}$",
+    regex=r"^[A-Z]{3}[0-9]{5}$",
     message="Invalid license number. Must be in the format: 'ABC1234'."
 )
